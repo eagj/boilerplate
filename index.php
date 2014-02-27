2ac4bd17b95f63a8a0f16e621dc88cba5fad7eef
eagj <quique@eagj.net>
2014-02-27 18:20:45 +0100

    primera subida

---
+++ index.php
@@ -0,0 +1,15 @@
+<DOCTYPE! html>
+<html>
+<head>
+	<meta charset="utf-8>">
+	<meta name="viewport" content="width=device-width">
+	<title>prueba</title>
+</head>
+
+<body>
+	<h1>hola k hase</h1>
+	<!--<img src="<?php bloginfo(template_url); ?>/screenshot.png"/>-->
+
+	<?php bloginfo('charset'); ?>
+</body>
+</html>
