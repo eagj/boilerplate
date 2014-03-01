0ce03d43fc9cb56b347035dae30a57678278dc5d
eagj <quique@eagj.net>
2014-03-01 12:40:32 +0100

    menuextra

--- plantilla-con-nav.php
+++ plantilla-con-nav.php
@@ -2,8 +2,7 @@
 /*
 Template Name: Plantilla con Nav
 */
- ?>
-
+?>
 
 <DOCTYPE! html>
 <html>
@@ -18,7 +17,8 @@ Template Name: Plantilla con Nav
 	<h1>Pagina(page.php): <?php the_title();?></h1>
 	<?php the_post();?>
 	<?php the_content( );?>
-	<a href="<?php bloginfo('home') ?>">Ir al inicio</a>
+	<a href="<?php bloginfo(home);?>">Ir al inicio</a>
 	
+<?php include("menu-extra.php") ?>
 </body>
 </html>
